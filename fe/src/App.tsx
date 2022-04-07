71f8b7b0f0afd2ff1d39fa7658a3ff394c9bc235
Christian Oehne <info@oehne.de>
2022-04-07 10:41:58 +0000

    Add RequireAuth component to private routes

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,8 +1,9 @@
-import { Outlet, Route, Routes } from "react-router-dom"
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom"
 import "./App.css"
 import FullPageError404 from "./components/FullPageError404"
 import FullPageErrorFallback from "./components/FullPageErrorFallback"
 import Navbar from "./components/Navbar"
+import { useAuth } from "./context/AuthContext"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import NewTrade from "./pages/NewTrade"
@@ -12,29 +13,33 @@ import Trade from "./pages/Trade"
 import TradeHistory from "./pages/Trades"
 import Transactions from "./pages/Transactions"
 
-//TODO:
-// - Handle the request for unauthorized routes
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+
+        {/* Public Routes */}
         <Route index element={<Home />} />
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
-        <Route path="transactions" element={<Transactions />} />
+
+        {/* Private Routes */}
+        <Route path="transactions" element={<RequireAuth><Transactions /></RequireAuth>} />
         <Route
           path="transactions/deposit"
-          element={<NewTsx type={"deposit"} />}
+          element={<RequireAuth><NewTsx type={"deposit"} /></RequireAuth>}
         />
         <Route
           path="transactions/withdraw"
-          element={<NewTsx type={"withdraw"} />}
+          element={<RequireAuth><NewTsx type={"withdraw"} /></RequireAuth>}
         />
-        <Route path="trade/:id" element={<Trade />} />
-        <Route path="tradehistory" element={<TradeHistory />} />
-        <Route path="trades/buy" element={<NewTrade type="buy" />} />
-        <Route path="trades/sell" element={<NewTrade type="sell" />} />
+        <Route path="trade/:id" element={<RequireAuth><Trade/></RequireAuth>} />
+        <Route path="tradehistory" element={<RequireAuth><TradeHistory/></RequireAuth>} />
+        <Route path="trades/buy" element={<RequireAuth><NewTrade type="buy" /></RequireAuth>} />
+        <Route path="trades/sell" element={<RequireAuth><NewTrade type="sell" /></RequireAuth>} />
+        
+        {/* Error Routes */}
         <Route path="*" element={<FullPageError404 />} />
         <Route
           path="error"
@@ -54,6 +59,21 @@ function Layout() {
       <Outlet />
     </div>
   )
+}
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const { user } = useAuth()
+  const location = useLocation();
+
+  if (!user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
 }
 
 export default App
